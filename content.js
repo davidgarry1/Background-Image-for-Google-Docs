bbfb6f132026168aed51b796395f124500f73faa
David Garry <dgarry@usc.edu>
2018-03-31 10:37:26 +0100

    added support for left outline bar

--- content.js
+++ content.js
@@ -1,22 +1,22 @@
 chrome.storage.sync.get({
-  photoTopics: 'nature,ocean,scenic,paradise,tropics,sky',
-  definition: false,
-  featured: true
+	photoTopics: 'nature,ocean,scenic,paradise,tropics,sky',
+	definition: false,
+	featured: true
 }, function(items) {
-  var url = "https://source.unsplash.com/1500x800/?";
-  if(items.definition){
-    url = "https://source.unsplash.com/3300x1600/?";
-  }
-  url += items.photoTopics;
-  if(items.featured){
-    url += ",featured";
-  }
-  if(document.getElementById("workspace-container") != null){
-    document.getElementById("workspace-container").style.backgroundImage = "url('" + url + "')";
-  }
-  if(document.getElementsByClassName("kix-appview-editor").length != 0){
-    for (var i = 0; i < document.getElementsByClassName("kix-appview-editor").length; i++) {
-        document.getElementsByClassName("kix-appview-editor")[i].style.backgroundImage = "url('" + url + "')";
-    }
-  }
+	var url = "https://source.unsplash.com/1500x800/?";
+	if (items.definition) {
+		url = "https://source.unsplash.com/3300x1600/?";
+	}
+	url += items.photoTopics;
+	if (items.featured) {
+		url += ",featured";
+	}
+	if (document.getElementById("workspace-container") != null) {
+		document.getElementById("workspace-container").style.backgroundImage = "url('" + url + "')";
+	}
+	if (document.getElementsByClassName("kix-appview-editor").length != 0) {
+		for (var i = 0; i < document.getElementsByClassName("kix-appview-editor").length; i++) {
+			document.getElementsByClassName("kix-appview-editor")[i].style.backgroundImage = "url('" + url + "')";
+		}
+	}
 });
--- options.js
+++ options.js
@@ -1,36 +1,34 @@
 // Saves options to chrome.storage
 function save_options() {
-  var topics = document.getElementById('topics').value;
-  var ultraHD = document.getElementById('uhd').checked;
-  var feat = document.getElementById('feat').checked;
-  chrome.storage.sync.set({
-    photoTopics: topics,
-    definition: ultraHD,
-    featured: feat
-  }, function() {
-    // Update status to let user know options were saved.
-    var status = document.getElementById('status');
-    status.textContent = 'Options saved.';
-    setTimeout(function() {
-      status.textContent = '';
-    }, 750);
-  });
+	var topics = document.getElementById('topics').value;
+	var ultraHD = document.getElementById('uhd').checked;
+	var feat = document.getElementById('feat').checked;
+	chrome.storage.sync.set({
+		photoTopics: topics,
+		definition: ultraHD,
+		featured: feat
+	}, function() {
+		// Update status to let user know options were saved.
+		var status = document.getElementById('status');
+		status.textContent = 'Options saved.';
+		setTimeout(function() {
+			status.textContent = '';
+		}, 750);
+	});
 }
-
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
 function restore_options() {
-  // Use default value color = 'red' and likesColor = true.
-  chrome.storage.sync.get({
-    photoTopics: 'nature,ocean,scenic,paradise,tropics,sky',
-    definition: false,
-    featured: true
-  }, function(items) {
-    document.getElementById('topics').value = items.photoTopics;
-    document.getElementById('uhd').checked = items.definition;
-    document.getElementById('feat').checked = items.featured;
-  });
+	// Use default value color = 'red' and likesColor = true.
+	chrome.storage.sync.get({
+		photoTopics: 'nature,ocean,scenic,paradise,tropics,sky',
+		definition: false,
+		featured: true
+	}, function(items) {
+		document.getElementById('topics').value = items.photoTopics;
+		document.getElementById('uhd').checked = items.definition;
+		document.getElementById('feat').checked = items.featured;
+	});
 }
 document.addEventListener('DOMContentLoaded', restore_options);
-document.getElementById('save').addEventListener('click',
-    save_options);
+document.getElementById('save').addEventListener('click', save_options);
